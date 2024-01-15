c85a11166720b9a7c99db425f6f28e77049d76c5
thunderswift59 <49886055+thunderswift59@users.noreply.github.com>
2024-01-15 14:23:17 -0600

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,5 @@
+module Mod-dependentPuppy
+
+go 1.21.0
+
+require github.com/thunderswift59/puppy v0.0.0-20240102014659-452f82df1681 // indirect
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/thunderswift59/puppy"
+)
+
+func main() {
+	fmt.Println("Hello")
+	fmt.Println(puppy.Bark())
+	fmt.Println(puppy.Barks())
+
+	fmt.Println("Git tags")
+
+	fmt.Println("blah blah blah blah")
+}
